76d15cdc227e41c20f7cc0df6b5faac0a7a4a1ff
Jennifer Franco <106487806+jfranco3@users.noreply.github.com>
2022-08-11 09:38:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect, useContext, useCallback} from 'react';
 
 
-
-<h1>John Lane</h1>
-
+function App() {
 
 
-
-  class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      
-      };
-    }
-    render() {
-      return (
-          <div className="App">
+  return (
+   
+  <div className="App">
             <h1>HELLO ITS JENN!</h1>
              <h1>Zak Kay</h1>
              <h1>I'm hungry</h1>
-          </div>
-      )
-    }
-  }
-  
-  export default App;
+             <h1>John Lane</h1>
+   
+  );
+}
+
+export default App;
+
+
 
 
