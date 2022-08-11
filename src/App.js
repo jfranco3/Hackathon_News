6b0eaa444fc2d057efa4d7a8111edcf07839ba91
Jenn Franco <jfranco376@gmail.com>
2022-08-11 16:46:02 -0500

    added articleCard, edit appComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect, useContext, useCallback } from "react";
+import React from "react";
+import AppComponent from "./Components/AppComponent";
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO ITS JENN!</h1>
-      <h1>Zak Kay</h1>
-      <h1>I'm hungry</h1>
-      <h1>John Lane</h1>
+      <AppComponent />
     </div>
   );
 }
