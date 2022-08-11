c7f0929a419333bf8705256bae4dadcdaa5a7ab4
Jenn Franco <jfranco376@gmail.com>
2022-08-11 15:02:40 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, {useState, useEffect, useContext, useCallback} from 'react';
-
+import React, { useState, useEffect, useContext, useCallback } from "react";
 
 function App() {
-
-
   return (
-   
-  <div className="App">
-            <h1>HELLO ITS JENN!</h1>
-             <h1>Zak Kay</h1>
-             <h1>I'm hungry</h1>
-             <h1>John Lane</h1>
-  </div>
-   
+    <div className="App">
+      <h1>HELLO ITS JENN!</h1>
+      <h1>Zak Kay</h1>
+      <h1>I'm hungry</h1>
+      <h1>John Lane</h1>
+    </div>
   );
 }
 
 export default App;
-
-
-
-
