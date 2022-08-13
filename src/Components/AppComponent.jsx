fab6149f6df78627916d5c7607602a90bc72aa84
AdamRLambert <Lambertar@gmail.com>
2022-08-13 11:55:52 -0700

    card and figuring it out

--- src/Components/AppComponent.jsx
+++ src/Components/AppComponent.jsx
@@ -6,6 +6,7 @@ import SearchForm from "./SearchForm";
 // a. Fetches data and holds it in state
 // b. Then passes as props to LIST ARTICLES
 // c. Passes methods as props to SEARCH FORM
+// This is jenn and I trying to figure this out
 
 export default function AppComponent() {
   const [stories, setStories] = useState([]);
