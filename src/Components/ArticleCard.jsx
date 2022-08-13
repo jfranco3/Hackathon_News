b4ff4a2cccec1e73cae3aa3f47d30fbb68a25b42
AdamRLambert <Lambertar@gmail.com>
2022-08-13 11:10:11 -0700

    competed card

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+
+import CardContent from "@mui/material/CardContent";
+
+import Typography from "@mui/material/Typography";
 
 export default function ArticleCard(props) {
   const { stories, setStories } = props;
   console.log("PROPS", props.stories);
-  return <div>ArticleCard</div>;
+
+  const handleClick = () => {
+    window.open(props.url);
+  };
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 14 }}
+          color="text.secondary"
+          gutterBottom
+          onClick={handleClick}
+        ></Typography>
+        <Typography variant="h5" component="div">
+          Title: {props.stories.titles}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Date: {props.stories.created_at}
+        </Typography>
+        <Typography variant="body2">Author: {props.stories.author}</Typography>
+        <Typography variant="body2">URL: {props.stories.url}</Typography>
+      </CardContent>
+
+      {/* <ArticleCard /> */}
+    </Card>
+  );
 }
