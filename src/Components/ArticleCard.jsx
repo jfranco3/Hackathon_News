cb4767d411795076941e7e8c6013a84564b5f1a0
AdamRLambert <Lambertar@gmail.com>
2022-08-13 16:41:54 -0700

    working card needs some styling

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 export default function ArticleCard(props) {
   const {
@@ -12,6 +17,31 @@ export default function ArticleCard(props) {
     objectId,
     points,
   } = props;
-  console.log("PROPS", props.stories);
-  return <div>{title}</div>;
+  // console.log("PROPS", props.stories);
+
+  const handleClick = () => {
+    window.open(url);
+  };
+
+  return (
+    <Paper elevation={3}>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography
+            variant="h5"
+            component="div"
+            onClick={handleClick}
+            style={{ cursor: "pointer" }}
+          >
+            {title}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {createdAt}
+          </Typography>
+          <Typography variant="body2">Author: {author}</Typography>
+          <Typography variant="body2">URL: {url}</Typography>
+        </CardContent>
+      </Card>
+    </Paper>
+  );
 }
