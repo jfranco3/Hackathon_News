d95b996b6b8be250008af0b6e68b8a5d9d683cce
ZakAKay <zakkay@ymail.com>
2022-08-13 16:18:07 -0500

    successfully stored user input

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+// import TextField from "@mui/material/TextField";
+//A) search by tag, date, author and title (see API documentation)
+//B) Uses method passed as props to store filtered data in API
 
 export default function SearchForm(props) {
+  const [input, setInput] = useState("");
   const { stories, setStories } = props;
-  return <div>SearchForm</div>;
+  // console.log("stories", stories);
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  };
+  
+  console.log("input", input);
+  return (
+    <div>
+      {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+      <form>
+        <label>
+          <input
+            placeholder="Search..."
+            type="text"
+            onChange={handleChange}
+            value={input}
+          
+          />
+        </label>
+      </form>
+    </div>
+  );
 }
